061eb89c35e8c77576fdf109b305e42e287d0fa2
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-02-21 15:51:23 +0100

    styling homework implementation

--- App.js
+++ App.js
@@ -3,26 +3,55 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello World</Text>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          Here are some boxes of different colours
+        </Text>
+      </View>
+      <View style={[styles.container, styles.cyan]}>
+        <Text style={styles.textArea}>Cyan #2aa198</Text>
+      </View>
+      <View style={[styles.container, styles.blue]}>
+        <Text style={styles.textArea}>Blue #268bd2</Text>
+      </View>
+      <View style={[styles.container, styles.magenta]}>
+        <Text style={styles.textArea}>Magenta #d33682</Text>
+      </View>
+      <View style={[styles.container, styles.orange]}>
+        <Text style={styles.textArea}>Orange #cb4b16</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
+  headerText: {
+    fontWeight: 'bold',
+    marginTop: 20,
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    borderRadius: 19,
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
   },
-  safeArea: {
-    flex: 2,
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  textArea: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
   },
 });
 
