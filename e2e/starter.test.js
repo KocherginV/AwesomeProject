a3744cd1e2aeaa53452290ef5f07c6b86f1fcd55
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-03-07 12:36:58 +0100

    finish config, add first e2e test

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -1,4 +1,6 @@
-describe('Example', () => {
+import { device, element, by } from 'detox';
+
+describe('Palettes tests', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -7,17 +9,7 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should have "home" heading', async () => {
+    await expect(element(by.label('Home'))).toBeVisible();
   });
 });
