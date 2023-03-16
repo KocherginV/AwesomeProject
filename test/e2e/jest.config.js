943e890b45259b336f59b0348b85c256c2fd2ca4
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-03-16 12:15:47 +0100

    update project structure

--- test/e2e/jest.config.js
+++ test/e2e/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  rootDir: '..',
-  testMatch: ['<rootDir>/e2e/**/*.test.js'],
+  rootDir: '../..',
+  testMatch: ['<rootDir>/test/e2e/**/*.test.js'],
   testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
